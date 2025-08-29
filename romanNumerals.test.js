740eed525910d285645088ec0c1ae3acd3f8a7ae
krist <kristoffer.b.fernholt@gmail.com>
2025-08-29 16:03:31 +0200

    idk what is going on just yet

---
+++ romanNumerals.test.js
@@ -0,0 +1,9 @@
+import { test, expect } from "vitest";
+
+function romanNumerals(number) {
+  return "I";
+}
+
+test("dette er det testen heter på en måte", () => {
+  expect(romanNumerals(1)).toBe("I");
+});
