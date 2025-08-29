7d58f767f9ce777642e3841f3f23fd77e1ac35c0
krist <kristoffer.b.fernholt@gmail.com>
2025-08-29 20:48:58 +0200

    setup project

--- romanNumbers.test.js
+++ romanNumbers.test.js
@@ -1,8 +1,22 @@
 import { test, expect } from "vitest";
 
 function romanNumerals(number) {
-  if (number === 2) return "II";
-  return "I";
+  let result = "";
+  if (number >= 10) {
+    result += "X";
+    number -= 10;
+  }
+  if (number === 9) return "IX";
+  if (number >= 5) {
+    result += "V";
+    number -= 5;
+  }
+  if (number === 4) return "IV";
+
+  for (let i = 0; i < number; i++) {
+    result += "I";
+  }
+  return result;
 }
 function testRomanNumerals(number, expected) {
   test(`${number} should be ${expected}`, () => {
@@ -11,3 +25,11 @@ function testRomanNumerals(number, expected) {
 }
 testRomanNumerals(1, "I");
 testRomanNumerals(2, "II");
+testRomanNumerals(3, "III");
+testRomanNumerals(4, "IV");
+testRomanNumerals(5, "V");
+testRomanNumerals(6, "VI");
+testRomanNumerals(9, "IX");
+testRomanNumerals(10, "X");
+testRomanNumerals(12, "XII");
+testRomanNumerals(15, "XV");
